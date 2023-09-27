3e34f392e4f8f2c59b07bf42e543a9b2a7a0ef54
Tetiana Zykova <tatiana.nv96@gmail.com>
2023-09-27 16:15:59 +0300

    init back

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const authRouter = require('./routes/api/auth');
+
+const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.set('trust proxy', true);
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/auth', authRouter);
+
+app.use((_, res) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: 'Use api on routes: /api/auth',
+    data: 'Not found',
+  });
+});
+
+app.use((err, _, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message });
+});
+
+module.exports = app;
