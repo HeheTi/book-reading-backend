484c756ca5d4ce6009855cf6c28bfb9465162ab4
Tetiana Zykova <tatiana.nv96@gmail.com>
2023-09-27 19:52:08 +0300

    initialize swagger

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const authRouter = require('./routes/api/auth');
 const { errorHandler, errorNotFoundHandler } = require('./helpers');
 
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const swaggerDocument = YAML.load('./data/swagger.yaml');
 
 app.set('trust proxy', true);
 app.use(logger(formatsLogger));
@@ -14,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/api-doks', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/users', authRouter);
 
 app.use(errorNotFoundHandler);
