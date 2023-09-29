89b9e632984851c4e310018b1a9c530c0ff9e09d
Tetiana Zykova <tatiana.nv96@gmail.com>
2023-09-29 16:34:54 +0300

    fix rout swagger

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api-doks', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/users', authRouter);
 
 app.use(errorNotFoundHandler);
