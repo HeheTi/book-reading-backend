4787dc109e2b949fed90110218d32cdc066b301c
Tetiana Zykova <tatiana.nv96@gmail.com>
2023-09-30 19:53:23 +0300

    add rout logout

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,9 +3,11 @@ const { ctrlWrapper } = require('../../helpers');
 const registration = require('./registration');
 const login = require('./login');
 const refresh = require('./refresh');
+const logout = require('./logout');
 
 module.exports = {
   registration: ctrlWrapper(registration),
   login: ctrlWrapper(login),
   refresh: ctrlWrapper(refresh),
+  logout: ctrlWrapper(logout),
 };
